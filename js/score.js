1b0211825fdebf779a7721f2ccb7a3fd52787a4e
Fanny Perret <fannyperretlombard@gmail.com>
2018-05-18 11:06:21 +0200

    still understanding what is wrong with my code... :cat:

--- js/score.js
+++ js/score.js
@@ -1,5 +1,15 @@
 function check() {
 
+  var question0 = document.quiz.question0.value;
+  var correct = ["C'est une bonne réponse !"];
+
+  if (question0 == "63") {
+    correct;
+  }
+
   document.getElementById("after_response").style.visibility = "hidden";
   document.getElementById("after_submit").style.visibility = "visible";
+
+  document.getElementById("rightOrWrong").innerHTML = correct;
+  document.getElementById("answer-0")
 }
