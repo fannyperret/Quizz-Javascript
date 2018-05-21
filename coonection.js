258e604f95c6eeabef7fe0fa0a6d2eb067dcd280
Fanny Perret <fannyperretlombard@gmail.com>
2018-05-21 12:14:14 +0200

    put an autofocus to the input quiz :cat:

--- coonection.js
+++ coonection.js
@@ -1,23 +1,15 @@
 function showHint(str) {
   if(str.length == 0) {
-    document.getElementById("")
+    document.getElementById("txtHint").innerHTML ="";
+    return;
+  } else {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if(this.readyState == 4 && this.status == 200) {
+        document.getElementById("txtHint").innerHTML = this.responseText;
+      }
+    };
+    xmlhttp.open("POST", "postint.php?q=" + str, true);
+    xmlhttp.send();
   }
-}
-
-
-
-function showHint(str) {
-    if (str.length == 0) {
-        document.getElementById("txtHint").innerHTML = "";
-        return;
-    } else {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("txtHint").innerHTML = this.responseText;
-            }
-        };
-        xmlhttp.open("GET", "gethint.php?q=" + str, true);
-        xmlhttp.send();
-    }
 }
