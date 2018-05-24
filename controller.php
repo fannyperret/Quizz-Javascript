d09b35436a1ba53b7e60e8a2445d334987cdc48a
Fanny Perret <fannyperretlombard@gmail.com>
2018-05-24 17:40:18 +0200

    quiz super quiz :cat:

--- controller.php
+++ controller.php
@@ -5,8 +5,9 @@ $questions = json_decode(file_get_contents('quiz.json'));
 // Holds the name of the method for a form submission to restrict your form procesing only form POST type request
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
-    $id = $_POST['id'];
-    $value = $_POST['value'];
+    // SECURITY
+    $id = htmlentities(trim(strip_tags($_POST['id'])));
+    $value = htmlentities(trim(strip_tags($_POST['value'])));
 
     if (isset($questions[$id])) {
         $question = $questions[$id];
@@ -19,35 +20,45 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
           ]);
 
         // IS_NUMERIC
-        } elseif (is_numeric($question->answer)) {
-            if ($question->answer != is_numeric($value)) {
-              echo json_encode([
-                "error" => "On met des chiffres !",
-                "id" => $id
-              ]);
+      } elseif ($question->type == "numeric") {
+          $value = str_replace(",", ".", $value);
+          $value = str_replace(" ", "", $value);
+          $value = (float)$value;
+          if (!preg_match('/^[0-9 \.,]+$/ui', $value)) {
+            echo json_encode([
+              "error" => "On met des chiffres !",
+              "id" => $id
+            ]);
+          } else {
+            $result = [
+                "id" => $id,
+                "title" => $question->titleAnswer,
+                "text" => $question->textAnswer,
+                "yourEstimation" => "",
+            ];
+            if ($value == $question->answer || (isset($question->aliasAnswer) && !empty($question->aliasAnswer) && in_array($value, $question->aliasAnswer))) {
+                $result["yourEstimation"] = "Vous avez répondu " . $value . " : c'est une bonne réponse !";
             } else {
-              $result = [
-                  "id" => $id,
-                  "title" => $question->titleAnswer,
-                  "text" => $question->textAnswer,
-                  "yourEstimation" => "",
-              ];
-              if ($value === $question->answer) {
-                  $result["yourEstimation"] = "Vous avez répondu " . $value . " : c'est une bonne réponse !";
-              } elseif ($value < $question->answer) {
-                  $result["yourEstimation"] = "Vous avez répondu " . $value . " : c'est PLUS ! Il y a un écart de " . ($question->answer - $value) . " avec la véritable réponse.";
-              } elseif ($value > $question->answer) {
-                  $result["yourEstimation"] = "Vous avez répondu " . $value . ": c'est MOINS ! Il y a un écart de " . ($value - $question->answer) . " avec la véritable réponse.";
-              } else {
-                  $result["yourEstimation"] = "Votre réponse est Improbable";
+              $estimation = null;
+              if(!isset($question->aliasAnswer)) $question->aliasAnswer = array();
+              $question->aliasAnswer[] = $question->answer;
+              foreach( $question->aliasAnswer as $k => $alias){
+                if ($value < $alias) {
+                  $estimation = "Vous avez répondu " . $value . " : c'est PLUS ! Il y a un écart de " . round(abs($question->answer - $value), 2) . " avec " . $question->answer;
+                } elseif ($value > $alias) {
+                  $estimation = "Vous avez répondu " . $value . ": c'est MOINS ! Il y a un écart de " . round(abs($question->answer - $value), 2) . " avec " . $question->answer;
+                }
+                if($estimation != null) break;
               }
-              echo json_encode($result);
+              $result["yourEstimation"] = $estimation;
             }
+            echo json_encode($result);
+          }
 
         // IS_LETTERS
-      } elseif (ctype_alpha($question->answer)) {
-        $value = ucfirst(strtolower(trim($value)));
-        if ($question->answer != ctype_alpha($value)) {
+      } elseif ($question->type == "alpha") {
+        // $value = ucfirst(strtolower(trim($value)));
+        if (!preg_match('/^[^!<>,;?=+()@#"°{}_$%:]+$/ui', stripslashes($value))) {
           echo json_encode([
             "error" => "On met des lettres !",
             "id" => $id
@@ -59,11 +70,22 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
               "text" => $question->textAnswer,
               "yourEstimation" => "",
           ];
-          if ($value === $question->answer) {
-            $result["yourEstimation"] = "Vous avez répondu " . $value . " : c'est EXACT !";
-          } else {
-            $result["yourEstimation"] = "Vous avez répondu " . $value . " : désolé ! Ce n'est pas la bonne réponse...";
+          $estimation = null;
+          if (strtolower($value) === $question->answer || preg_match('/'.$question->answer.'/', $value)  || (isset($question->aliasAnswer) && !empty($question->aliasAnswer) && in_array(strtolower($value), $question->aliasAnswer))) {
+            $estimation = true;
+          } elseif(isset($question->aliasAnswer) && !empty($question->aliasAnswer)) {
+            if(!isset($question->aliasAnswer)) $question->aliasAnswer = array();
+            $question->aliasAnswer[] = $question->answer;
+            foreach( $question->aliasAnswer as $k => $alias){
+              if (preg_match('#'.addslashes($alias).'#ui', $value)) {
+                $estimation = true;
+                break;
+              }
+            }
           }
+          if($estimation == null) $result["yourEstimation"] = "Vous avez répondu " . $value . " : désolé ! La bonne réponse était : " . $question->answer;
+          else $result["yourEstimation"] = "Vous avez répondu " . $value . " : c'est EXACT !";
+
           echo json_encode($result);
         }
 
